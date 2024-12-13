3819a0250848041c5b826c2370cfd7ecf694cbd0
Devlin Duldulao <webmasterdevlin@gmail.com>
2024-12-13 09:53:20 +0700

    update

---
+++ app.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "@tanstack/start/config";
+import tsConfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  vite: {
+    plugins: [
+      tsConfigPaths({
+        projects: ["./tsconfig.json"],
+      }),
+    ],
+  },
+
+  server: {
+    // https://tanstack.com/router/latest/docs/framework/react/start/hosting#deployment
+    preset: "bun",
+  },
+});
