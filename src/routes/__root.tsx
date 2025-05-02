58d453ea655fa676c0975ddb5749229f717a4452
Devlin Duldulao <webmasterdevlin@gmail.com>
2025-05-02 15:39:11 +0200

    fix: correct import path for TanStackRouterDevtools and update package dependencies

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -29,7 +29,7 @@ const TanStackRouterDevtools =
     ? () => null // Render nothing in production
     : lazy(() =>
       // Lazy load in development
-      import('@tanstack/router-devtools').then((res) => ({
+      import('@tanstack/react-router-devtools').then((res) => ({
         default: res.TanStackRouterDevtools,
         // For Embedded Mode
         // default: res.TanStackRouterDevtoolsPanel
